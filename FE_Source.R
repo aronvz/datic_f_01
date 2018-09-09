ff3289ef888432cbdac2b6b6bdcb7694966a9882
Pam <pamela.inostroza@datic.cl>
2018-09-09 12:55:55 -0300

    como ruta

--- FE_Source.R
+++ FE_Source.R
@@ -1,5 +1,5 @@
 library(readr)
-#The master Databases are in "Proyectos ejecucion, 2018, F Emilia."
+#The master Databases are in "Proyectos en ejecucion\\2018\\Fundacion Emilia"
 EGR2014 <- read_delim("D:/Work/Datic/FEmilia_Datos/SSegreso2014/egr_2014.csv", 
                       ";", escape_double = FALSE, trim_ws = TRUE)
 
