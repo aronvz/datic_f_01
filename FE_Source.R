68b577d859d508bf9a9f221b9470b8177e26d18c
Aharon Valdes <a.valdes@uc.cl>
2018-09-09 13:02:54 -0300

    #add database file references.

--- FE_Source.R
+++ FE_Source.R
@@ -1,4 +1,5 @@
 library(readr)
+#The master Databases are in "Proyectos ejecucion, 2018, F Emilia."
 EGR2014 <- read_delim("D:/Work/Datic/FEmilia_Datos/SSegreso2014/egr_2014.csv", 
                       ";", escape_double = FALSE, trim_ws = TRUE)
 
