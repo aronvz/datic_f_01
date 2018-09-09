087a60a21d08798c284c8db734ec5056fc7ee2fd
Aharon Valdes <a.valdes@uc.cl>
2018-09-09 00:09:58 -0300

    new

---
+++ FE_Source.R
@@ -0,0 +1,6 @@
+library(readr)
+EGR2014 <- read_delim("D:/Work/Datic/FEmilia_Datos/SSegreso2014/egr_2014.csv", 
+                      ";", escape_double = FALSE, trim_ws = TRUE)
+
+DEF2014 <- read_delim("D:/Work/Datic/FEmilia_Datos/SSPais2014/DEF2014.csv", 
+                      ";", escape_double = FALSE, trim_ws = TRUE)
