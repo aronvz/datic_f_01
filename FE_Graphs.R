d62c5ecf007b846fa91aed4231b80739cf3ef450
Aharon Valdes <a.valdes@uc.cl>
2018-09-29 21:36:27 -0300

    msss

--- FE_Graphs.R
+++ FE_Graphs.R
@@ -1,50 +1,53 @@
-library(readr)
+
 library(ggplot2)
 library(plyr)
 library(reshape2)
 
-source("FE_Source.R")
+syntaxdir <- c("C:\\Users\\pamel\\Google Drive\\DATIC\\Emilia\\datic_f_01\\")
+source(paste0(syntaxdir,"FE_Source.R"))
 
 str(EGR2014)
+summary(EGR2014)
 
 str(DEF2014)
 summary(DEF2014)
-class(DEF2014)
-
-vector9 <- c('SEXO','EST_CIVIL','EDAD_TIPO','CURSO_INS','NIVEL_INS','ACTIV','CATEG','AT_MEDICA',
-             'CAL_MEDICO','NUTRITIVO')
-vector99 <- c('GESTACION','REG_RES','MES_NAC','ANO1_NAC','ANO2_NAC','SERV_RES')
-vector999 <- c('EDAD_CANT')
-vector9999 <- c('PESO')
-vector99999 <- c('COMUNA')
-
-DEF2014[ , as.vector(vector9)][ DEF2014[ , as.vector(vector9) ] == 9 ] <- NA
-DEF2014[ , as.vector(vector99)][ DEF2014[ , as.vector(vector99) ] == 99 ] <- NA
-DEF2014[ , as.vector(vector999)][ DEF2014[ , as.vector(vector999) ] == 999 ] <- NA
-DEF2014[ , as.vector(vector9999)][ DEF2014[ , as.vector(vector9999) ] == 9999 ] <- NA
-DEF2014[ , as.vector(vector99999)][ DEF2014[ , as.vector(vector99999) ] == 99999 ] <- NA
-
-summary(DEF2014)
-DEF2014$MES_DEF <- factor(DEF2014$MES_DEF,levels = c(1:12), labels=c('Enero','Febrero','Marzo',
-                                                                        'Abril', 'Mayo','Junio',
-                                                                        'Julio', 'Agosto', 'Septiembre',
-                                                                        'Octubre', 'Noviembre', 'Diciembre'))
-DEF2014$SEXO <- factor(DEF2014$SEXO,levels = c(1,2), labels=c('Hombre','Mujer'))
-DEF2014$URBA_RURAL <- factor(DEF2014$URBA_RURAL,levels = c(1,2), labels=c('Urbano','Rural'))
-barplot(table(DEF2014$SEXO, DEF2014$MES_DEF), main = 'Defunciones por Mes y Sexo',legend.text = c('Hombre','Mujer'))
-
-library(dplyr)
-library(reshape2)
+# 
+# vector9 <- c('SEXO','EST_CIVIL','EDAD_TIPO','CURSO_INS','NIVEL_INS','ACTIV','CATEG','AT_MEDICA',
+#              'CAL_MEDICO','NUTRITIVO')
+# vector99 <- c('GESTACION','REG_RES','MES_NAC','ANO1_NAC','ANO2_NAC','SERV_RES')
+# vector999 <- c('EDAD_CANT')
+# vector9999 <- c('PESO')
+# vector99999 <- c('COMUNA')
+# 
+# DEF2014[ , as.vector(vector9)][ DEF2014[ , as.vector(vector9) ] == 9 ] <- NA
+# DEF2014[ , as.vector(vector99)][ DEF2014[ , as.vector(vector99) ] == 99 ] <- NA
+# DEF2014[ , as.vector(vector999)][ DEF2014[ , as.vector(vector999) ] == 999 ] <- NA
+# DEF2014[ , as.vector(vector9999)][ DEF2014[ , as.vector(vector9999) ] == 9999 ] <- NA
+# DEF2014[ , as.vector(vector99999)][ DEF2014[ , as.vector(vector99999) ] == 99999 ] <- NA
+# 
+# summary(DEF2014)
+# DEF2014$URBA_RURAL <- factor(DEF2014$URBA_RURAL,levels = c(1,2), labels=c('Urbano','Rural'))
+# barplot(table(DEF2014$SEXO, DEF2014$MES_DEF), main = 'Defunciones por Mes y Sexo',legend.text = c('Hombre','Mujer'))
+# 
+# library(dplyr)
+# library(reshape2)
 
 #mutate(sum5=count(rownames(.)))
 #melted <- melt(DEF2014[c("MES_DEF", "SEXO")], id=c("MES_DEF", "SEXO"))
 
-ca <- dcast(DEF2014, MES_DEF~SEXO,sum)
-print(ca)
-
 #def_sex_mes <- DEF2014 %>% 
 #  group_by(SEXO,MES_DEF)%>% tally()
 #  mutate(perc=count/sum(count))
+
+
+DEF2014$MES_DEF <- factor(DEF2014$MES_DEF,levels = c(1:12), labels=c('Enero','Febrero','Marzo',
+                                                                        'Abril', 'Mayo','Junio',
+                                                                        'Julio', 'Agosto', 'Septiembre',
+                                                                        'Octubre', 'Noviembre', 'Diciembre'))
+DEF2014$SEXO <- factor(DEF2014$SEXO,levels = c(1,2), labels = c('Hombre','Mujer'))
+
+ca <- dcast(DEF2014, MES_DEF~SEXO,sum)
+print(ca)
 
   
 def_sex_mes <- DEF2014 %>% 
